77d73d03cdd925ff3da3a16325ba61d136eaf4b8
wkdwnsghk <76817073+wkdwnsghk@users.noreply.github.com>
2021-02-14 20:30:29 +0900

    Update car_helpers.py

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -161,7 +161,7 @@ def get_car(logcan, sendcan, has_relay=False):
 
   if candidate is None:
     cloudlog.warning("car doesn't match any fingerprints: %r", fingerprints)
-    candidate = "mock"
+    candidate = "KIA SELTOS 2019"
 
   CarInterface, CarController, CarState = interfaces[candidate]
   car_params = CarInterface.get_params(candidate, fingerprints, has_relay, car_fw)
